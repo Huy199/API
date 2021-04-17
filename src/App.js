e9d9ef46fea14352e8a312e3aff82ec794946196
Huy199 <67187419+Huy199@users.noreply.github.com>
2021-04-17 19:11:43 +0700

    17/4/2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import './App.css';
+import Menu from './components/Menu/Menu';
+import List from './components/ProductList/List';
+import routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Menu />
+          <div className='container'>
+            <div className='row'>
+              {/* <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+              <button type='button' className='btn btn-info mb-10'>
+                Them san pham
+              </button>
+              <List />
+            </div> */}
+
+              {this.showContentMenus(routes)}
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+  showContentMenus = (routes) => {
+    var result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        );
+      });
+    }
+
+    return <Switch>{result}</Switch>;
+  };
 }
 
 export default App;
